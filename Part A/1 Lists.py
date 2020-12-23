c63838cb82b410e4dc9b1eafb71bf5477b246d38
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2020-12-23 23:50:58 +0530

    formatting changes

--- Part A/1 Lists.py
+++ Part A/1 Lists.py
@@ -1,24 +1,33 @@
-a=[]
-size=int(input("Enter size of list"))
+#empty list
+a = []
+
+size = int(input("Enter size of list"))
+
 for i in range(0,size):
-	ele=int(input("enter elements of list"))
+	ele = int(input("enter elements of list"))
 	a.append(ele)
+
+#OR
+a = [* map(int,input("Enter elements of list").split())]
+
 print("The list contains",a)
+
 mini=min(a)
 maxi=max(a)
 print("Minimum and maximum elements are :", mini, maxi)
 
-ele=int(input("Enter new element to be inserted"))
-pos=int(input("Enter position of the element"))
+ele = int(input("Enter new element to be inserted"))
+pos = int(input("Enter position of the element"))
 a.insert(pos,ele)
 
 print("The modified list as follows:",a)
 
-ele=int(input("Enter element to be removed"))
+ele = int(input("Enter element to be removed"))
 a.remove(ele)
 print("The modified list as follows:",a)
 
-ele=int(input("Enter element to be found"))
+ele = int(input("Enter element to be found"))
+
 if ele in a:
 	print("Element is found at",a.index(ele))
 else:
--- Part A/2 Rectangle.py
+++ Part A/2 Rectangle.py
@@ -4,6 +4,13 @@ class Rectangle:
 		self.b = int(input("Enter breadth"))
 	def area(self):
 		print("The area of the given rectangle is",self.l*self.b)
+	
+	def permimerter(self,a:int)->bool:
+		if(self.l + self.b > a):
+			return True
+		else:
+			return False
+
 
 R=Rectangle()
 R.area()
